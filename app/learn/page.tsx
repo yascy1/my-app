810df35a0179d347a840b2ac81cc4ae139c31357
yascy1 <s.kesumaningtyas.l@gmail.com>
2026-04-20 12:04:42 +0800

    latihan membuat halaman page pertemuan 5

---
+++ app/learn/page.tsx
@@ -0,0 +1,15 @@
+export default function learnPage() {
+    return (
+    <div>
+        <div> 
+            <img src="/palette.svg" alt="Design"/>
+            <p> Design </p>
+            </div>
+        
+        <div>
+        <p> Figma </p>
+        <p> 8 Hours ago</p>
+         </div>
+    </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Hi, Yascy
           </a>
         </div>
       </main>
