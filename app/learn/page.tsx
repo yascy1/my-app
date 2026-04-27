85aa6e4c9ff8d2563dcc4b7a9b8cdf090fc67453
yascy1 <s.kesumaningtyas.l@gmail.com>
2026-04-27 12:22:18 +0800

    Pertemuan 6 - Belajar Design CSS

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,15 +1,46 @@
 export default function learnPage() {
     return (
-    <div>
-        <div> 
-            <img src="/palette.svg" alt="Design"/>
-            <p> Design </p>
+    <section className="h-screen bg-gray-100 flex gap-4">
+        <div className="bg-purple-300 p-8 h-72 w-96 rounded-4xl flex flex-col justify-between text-blue-950">
+            <div className=" flex gap-4">
+                <img src="/palette.svg" alt="Design"/>
+                <p className="font-bold"> Design </p>
             </div>
         
-        <div>
-        <p> Figma </p>
-        <p> 8 Hours ago</p>
+            <div>
+                <h2 className="text-4xl font-bold"> Figma </h2>
+             
+             <p className="font-bold"> 8 Hours ago</p>
+            </div>
+        </div>
+
+
+        <div className="bg-orange-300 p-8 h-72 w-96 rounded-4xl flex flex-col justify-between text-blue-950">
+            <div className=" flex gap-4">
+                <img src="/code.svg" alt="Design"/>
+                <p className="font-bold"> Codding </p>
+            </div>
+        
+            <div>
+             <h2 className="text-4xl font-bold"> Phyton </h2>
+             
+             <p className="font-bold"> 2 days ago</p>
+            </div>
          </div>
-    </div>
+
+         
+         <div className="bg-red-300 p-8 h-72 w-96 rounded-4xl flex flex-col justify-between text-blue-950">
+            <div className=" flex gap-4">
+                <img src="/palette.svg" alt="Design"/>
+                <p className="font-bold"> Design </p>
+            </div>
+        
+            <div>
+                <h2 className="text-4xl font-bold"> Sketch </h2>
+             
+             <p className="font-bold"> 4 days ago</p>
+            </div>
+         </div>
+    </section>
     );
 }
